5fe8dd948993445cb5da7d5de1c7d41382d85f74
Diego Fialho Rodrigues <dfialho84@gmail.com>
2020-04-13 20:01:02 -0300

    traducao

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-importScripts("/precache-manifest.a8c7a81d3c3d43f8c0d1a9e4e2fef85f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.19df265b47b66af49678c6571b356b01.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // v1
 
-workbox.core.setCacheNameDetails({prefix: "gwt-brisco"});
+workbox.core.setCacheNameDetails({ prefix: "gwt-brisco" });
 
 /*self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -14,8 +14,8 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 self.addEventListener("message", msg => {
-    if(msg.data.action == "skipWaiting") {
-        self.skipWaiting();
-    }
+  if (msg.data.action == "skipWaiting") {
+    self.skipWaiting();
+  }
 });
 
